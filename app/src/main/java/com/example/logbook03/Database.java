c647dcf5b9788685feb3df9827276a97b1830ab0
Bui Ngoc Huy <buingochuy124@gmail.com>
2022-11-14 10:57:50 +0700

    last commit

--- app/src/main/java/com/example/logbook03/Database.java
+++ app/src/main/java/com/example/logbook03/Database.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class Database extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "Logbook.db";
+    public static final String DATABASE_NAME = "Logbook03.db";
     public static final int DATABASE_VERSION = 1;
     public static final String TABLE_NAME = "image_src";
     public static final String COLUMN_ID = "_id";
